9ffb5253d5720fc103cc9453f0bfa3033ec32931
201415053 김경린 <kilin94@naver.com>
2017-06-26 14:40:06 +0900

    Delete TurtleComparator.java

--- com/sd/sort/v2/TurtleComparator.java
+++
@@ -1,8 +0,0 @@
-package com.sd.sort.v2;
-import com.sd.turtle.WeightTurtle;
-
-public class TurtleComparator implements Comparator {
-    public int compare(Object t1, Object t2) {
-        return ((WeightTurtle)t1).getWeight()-((WeightTurtle)t2).getWeight();
-    }
-}
